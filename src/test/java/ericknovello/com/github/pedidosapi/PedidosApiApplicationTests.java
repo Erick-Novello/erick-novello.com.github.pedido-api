d978caa2ccdef308e3e8f4578bd939ed086360b9
Erick Novello <novello.erick@gmail.com>
2023-08-27 15:39:31 -0300

    first commit

---
+++ src/test/java/ericknovello/com/github/pedidosapi/PedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ericknovello.com.github.pedidosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
